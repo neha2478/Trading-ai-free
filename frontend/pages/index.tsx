2193d51c1a423a8da25322c3cf950180ef426c5c
Shruti Karmakar <84584037+neha2478@users.noreply.github.com>
2025-07-16 17:10:03 +0530

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,1 +1,15 @@
-#This is index file
+//This is index file
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Trading AI Free</title>
+      </Head>
+      <main className="h-screen flex items-center justify-center bg-gray-50">
+        <h1 className="text-4xl font-bold">Welcome to Trading AI Free!</h1>
+      </main>
+    </>
+  )
+}
