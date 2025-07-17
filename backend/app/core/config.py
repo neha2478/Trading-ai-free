0dcdee368d770cf330a895c4fadf66c4c9cebb60
Shruti Karmakar <84584037+neha2478@users.noreply.github.com>
2025-07-17 20:21:15 +0530

    Update config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,1 +1,16 @@
 #This is config file
+from pydantic import BaseSettings
+from typing import List
+
+class Settings(BaseSettings):
+    ALLOWED_ORIGINS: List[str]
+    ALPHAVANTAGE_KEY: str
+    FYERS_CLIENT_ID: str
+    FYERS_CLIENT_SECRET: str
+    KITE_API_KEY: str
+    KITE_API_SECRET: str
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
