30f67c716bdef8147bf1ab37b41c2bd15e1d4bc0
Shruti Karmakar <84584037+neha2478@users.noreply.github.com>
2025-07-17 20:29:03 +0530

    Update broker_kite.py

--- backend/app/services/broker_kite.py
+++ backend/app/services/broker_kite.py
@@ -1,1 +1,10 @@
-#this of broker kite connection
+#This of broker kite connection
+from kiteconnect import KiteConnect
+
+def kite_auth(api_key: str, api_secret: str) -> str:
+    kite = KiteConnect(api_key=api_key)
+    # user must follow login flow to get request_token
+    request_token = "...GET_FROM_UI..."
+    data = kite.generate_session(request_token, api_secret=api_secret)
+    return data["access_token"]
+
