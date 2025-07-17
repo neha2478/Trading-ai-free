b41f4db2cd2d5000eef0b8ebb5fadd57d5f1ad65
Shruti Karmakar <84584037+neha2478@users.noreply.github.com>
2025-07-17 20:22:26 +0530

    Update schemas.py

--- backend/app/models/schemas.py
+++ backend/app/models/schemas.py
@@ -1,1 +1,19 @@
 #This is schema's file
+from pydantic import BaseModel
+from typing import Optional, List
+
+class ScreenerResponse(BaseModel):
+    symbol: str
+    price: float
+
+class BrokerIn(BaseModel):
+    provider: str
+    name: Optional[str]
+    api_key: Optional[str]
+    api_secret: Optional[str]
+    oauth_token: Optional[str]
+
+class BrokerOut(BrokerIn):
+    id: str
+    primary: bool
+    status: str
